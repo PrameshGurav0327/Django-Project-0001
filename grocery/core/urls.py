fb80149274aab38667b7af5c4aec470fa7b1782b
Pramesh Gurav <prameshgurav79286@gmail.com>
2025-04-08 11:57:54 +0530

    update

--- grocery/core/urls.py
+++ grocery/core/urls.py
@@ -8,16 +8,27 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.index,name='index'),
+    
     path("products/",views.products,name='products'),
+    
     path("about/",views.about,name='about'),
+    
     path("contact/",views.contact,name="contact"),
+    
     path('product_details/<int:id>/',views.product_details,name='product_details'),
+    
     path("cart/",views.cart,name="cart"),
+    
     path("view_cart/",views.view_cart,name="view_cart"),
+    
     path('foodcategories/',views.foodcategories,name='foodcategories'),
+    
     path('kitchencategories/',views.kitchencategories,name='kitchencategories'),
+    
     path('instantfoodcategories/',views.instantfoodcategories,name='instantfoodcategories'),
+    
     path('fruitscategories/',views.fruitscategories,name='fruitscategories'),
+    
 ]
 
 if settings.DEBUG:
--- grocery/core/views.py
+++ grocery/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from . models import Grocery
+from . models import Grocery, Cart
 from . forms import RegistrationForm,AuthenticateForm,ChangePasswordForm,UserProfileForm,AdminProfileForm
 from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
 from django.contrib import messages
@@ -19,9 +19,6 @@ def about(request):
 def contact(request):
     return render(request,'core/contact.html')
 
-def product_details(request):
-    return render(request,'../templates/core/product_details.html')
-
 def cart(request):
     return render(request,'core/cart.html')
 
@@ -40,7 +37,57 @@ def kitchencategories(request):
 def instantfoodcategories(request):
     return render(request,"core/instantfoodcategories.html")
 
-def product_details(request,id):
-    GC= Grocery.objects.get(pk=id)
-    return render(request,'core/product_details.html',{'GC':GC})
+def product_details(request, id):
+    GC = get_object_or_404(Grocery, pk=id)
+    return render(request, '../core/product_details.html', {'GC': GC})
+
+# =============== Add to cart and Delete item ==============
+
+def add_to_cart(request,id):
+    if request.user.is_authenticated:
+        pet = Grocery.objects.get(pk=id)
+        user = request.user
+        Cart(user=user,product=pet).save()
+        messages.success(request,'Added to cart succcefully !')
+        return redirect('petdetails', id)
+    else:
+        return redirect('login')
+
+
+def view_cart(request):
+    if request.user.is_authenticated:
+        cart_items =Cart.objects.filter(user=request.user)
+        total=0
+        delhivery_charge=2000
+        for item in cart_items:
+            total+=(item.product.discounted_price*item.quantity)
+        final_price =total+delhivery_charge
+        return render(request,'core/view_cart.html',{'cart_items':cart_items,'total':total,'final_price':final_price})
+    else:
+        return redirect('login')
+
+
+def add_quantity(request,id):
+    if request.user.is_authenticated:
+        product = get_object_or_404(Cart,pk=id)
+        product.quantity+=1
+        product.save()
+        return redirect('viewcart')
+    else:
+        return redirect('login')
+
+def delete_quantity(request,id):
+    if request.user.is_authenticated:
+        product = get_object_or_404(Cart,pk=id)
+        if product.quantity>1:
+            product.quantity-=1
+            product.save()
+        return redirect('viewcart')
+    else:
+        return redirect('login')
+    
+def delete_cart(request,id):
+    pet_cart =Cart.objects.get(pk=id)
+    pet_cart.delete()
+    return redirect('viewcart')
 
